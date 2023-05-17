0bf830401f1d1ea32fb9cfe96368c12609d2ec1b
longnh257 <hoanglong020597@gmail.com>
2023-05-17 18:15:47 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import axios from "axios"
 
 
 let API_URL = localStorage.getItem("API_URL")
-axios.defaults.baseURL = API_URL ? API_URL : import.meta.env.VITE_APP_API_DEV
+axios.defaults.baseURL = API_URL==import.meta.env.VITE_APP_API_LIVE ? API_URL : import.meta.env.VITE_APP_API_DEV
 axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem("access_token")
 axios.defaults.headers.common['lang'] = 'vi'
 
